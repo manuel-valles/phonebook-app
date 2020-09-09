24ab0c79cabf258cb2bde2931a64b5005280654b
Manuel Valles <manukempo@gmail.com>
2020-09-09 13:33:27 +0100

    First approach for the list of Contacts

--- src/routers/user.js
+++ src/routers/user.js
@@ -65,4 +65,74 @@ router.delete('/user/me', auth, async (req, res) => {
   }
 })
 
+router.post('/user/contact', auth, async (req, res) => {
+  const user = req.user
+  const contact = user.contacts.find(contact => contact.phone === req.body.phone)
+  try {
+    if(!contact) {
+      user.contacts.push({
+        name: req.body.name, 
+        phone: req.body.phone
+      })
+      await user.save()
+      res.status(201).send(user)
+    } else {
+      res.status(404).send({ error: `The phone number already exists in your list of contacts with the name: ${contact.name}` })
+    }
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
+router.get('/user/contacts', auth, async (req, res) => {
+  try {
+    res.send(req.user.contacts)
+  } catch (error) {
+    res.status(404).send()
+  }
+})
+
+router.patch('/user/contact/:id', auth, async (req, res) => {
+  const user = req.user
+  const index = user.contacts.findIndex(contact => contact._id == req.params.id)
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'phone']
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+  
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+  
+  try {
+    if(index > -1){
+      user.contacts.map(contact => {
+        if(contact._id == req.params.id) updates.forEach(update => contact[update] = req.body[update])
+      })
+      await user.save()
+      res.send(user)
+    } else {
+      res.status(404).send({ error: 'Contact not found' })
+    }
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
+router.delete('/user/contact/:id', auth, async (req, res) => {
+  const user = req.user
+  const index = user.contacts.findIndex(contact => contact._id == req.params.id)
+
+  try {
+    if(index > -1) {
+      user.contacts.splice(index, 1)
+      await user.save()
+      res.send(user)
+    } else {
+      res.status(404).send({ error: 'Contact not found' })
+    }
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
 module.exports = router
